9624574c200bfeadc3917bfb4e91ca29dd9cbcae
John M. Wargo <johnwargo@users.noreply.github.com>
2023-03-19 07:00:17 -0400

    getting started

---
+++ .eleventy.js
@@ -0,0 +1,104 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+// https://github.com/11ty/eleventy/issues/2301
+const markdownIt = require("markdown-it");
+const markdownItAttrs = require("markdown-it-attrs");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = eleventyConfig => {
+
+	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+	eleventyConfig.addPlugin(pluginRss);
+	eleventyConfig.addPlugin(syntaxHighlight);
+
+	// https://github.com/11ty/eleventy/issues/2301
+	const mdOptions = {
+    html: true,
+    breaks: true,
+    linkify: true,
+  };
+  const markdownLib = markdownIt(mdOptions)
+    .use(markdownItAttrs)
+    .disable("code");
+
+  eleventyConfig.setLibrary("md", markdownLib);
+
+	// From ray camden's blog, first paragraph as excerpt
+	eleventyConfig.addShortcode('excerpt', post => extractExcerpt(post));
+	function extractExcerpt(post) {
+		if (!post.templateContent) return '';
+		if (post.templateContent.indexOf('</p>') > 0) {
+			let end = post.templateContent.indexOf('</p>');
+			return post.templateContent.substr(0, end + 4);
+		}
+		return post.templateContent;
+	}
+
+	eleventyConfig.addCollection("categories", function (collectionApi) {
+		let categories = new Set();
+		let posts = collectionApi.getFilteredByTag('post');
+		posts.forEach(p => {
+			let cats = p.data.categories;
+			cats.forEach(c => categories.add(c));
+		});
+		return Array.from(categories);
+	});
+
+	eleventyConfig.addFilter("filterByCategory", function (posts, cat) {
+		// case matters, so let's lowercase the desired category, cat	and we will 
+		// lowercase our posts categories as well
+		cat = cat.toLowerCase();
+		let result = posts.filter(p => {
+			let cats = p.data.categories.map(s => s.toLowerCase());
+			return cats.includes(cat);
+		});
+		return result;
+	});
+
+	// https://www.raymondcamden.com/2020/06/24/adding-algolia-search-to-eleventy-and-netlify
+	// Remove <code>.*</code>, remove HTML, then with plain text, limit to 5k chars
+	eleventyConfig.addFilter('algExcerpt', function (text) {
+		//first remove code
+		text = text.replace(/<code class="language-.*?">.*?<\/code>/sg, '');
+		//now remove html tags
+		text = text.replace(/<.*?>/g, '');
+		//now limit to 5k
+		return text.substring(0, 5000);
+	});
+
+	eleventyConfig.addFilter('jsonify', function (variable) {
+		return JSON.stringify(variable);
+	});
+
+	eleventyConfig.addPassthroughCopy("src/_data/*");
+	eleventyConfig.addPassthroughCopy("src/favicon/*");
+	eleventyConfig.addPassthroughCopy("src/assets/css/*");
+	eleventyConfig.addPassthroughCopy("src/assets/js/*");
+	eleventyConfig.addPassthroughCopy("src/assets/sass/*");
+	eleventyConfig.addPassthroughCopy("src/assets/webfonts/*");
+	// Images folders, assumes cascading folders per year
+	eleventyConfig.addPassthroughCopy("src/images/*");	
+	eleventyConfig.addPassthroughCopy("src/images/2009/*");	
+	eleventyConfig.addPassthroughCopy("src/images/2010/*");	
+	eleventyConfig.addPassthroughCopy("src/images/2011/*");	
+	eleventyConfig.addPassthroughCopy("src/images/2012/*");	
+	eleventyConfig.addPassthroughCopy("src/images/2013/*");	
+	eleventyConfig.addPassthroughCopy("src/images/2014/*");	
+	eleventyConfig.addPassthroughCopy("src/images/2015/*");	
+	eleventyConfig.addPassthroughCopy("src/images/2016/*");	
+	eleventyConfig.addPassthroughCopy("src/images/2017/*");	
+	eleventyConfig.addPassthroughCopy("src/images/2018/*");	
+	eleventyConfig.addPassthroughCopy("src/images/2019/*");	
+	eleventyConfig.addPassthroughCopy("src/images/2020/*");	
+	eleventyConfig.addPassthroughCopy("src/images/2021/*");	
+	eleventyConfig.addPassthroughCopy("src/images/2022/*");	
+	eleventyConfig.addPassthroughCopy("src/images/2023/*");	
+
+	return {
+		dir: {
+			input: 'src',
+			output: "_site",
+		}
+	}
+
+};
--- readme.md
+++ readme.md
@@ -2,15 +2,23 @@
 
 The static site generated version of [johnwargo.com](https://johnwargo.com), implemented using [Eleventy](https://www.11ty.dev/).
 
-I haven't even started on this thorny beast, but created this repository to track tasks.
-
 * [ ] Implement core 11ty project
 * [ ] Select template and implement (elemental, faction, )
+* [ ] Syntax highlights
 * [ ] Copy posts to project
 * [ ] Copy image files to project
 * [ ] Publish site on Netlify
+* [ ] Use jmw-test.com domain
+* [ ] RSS feed and link in menu
+* [ ] Categories page with descriptions
+* [ ] Sitemap (all pages && posts)
 * [ ] Section - Publications (books, articles, etc. from johnwargobooks.com)
 * [ ] Section - Code projects (from johnwargo.dev)
 * [ ] Section - Related Sites (all of the other sites I maintain)
+* [ ] Section - Sightings
 * [ ] Add Buy Me a Coffee
-* [ ] Add site search using, I think, Algolia
+* [ ] Algolia Search
+* [ ] Minification
+* [ ] Newsletter sign up
+* [ ] Automatically tweet and mastodon new posts?
+* [ ] Discussions
