c974c983282c46b4406f97392b3c5ec9af463d47
John M. Wargo <johnwargo@users.noreply.github.com>
2023-04-28 07:27:09 -0400

    Incrementing package version

--- pub.mjs
+++ pub.mjs
@@ -1,31 +1,44 @@
 #!/usr/bin/env zx
 
-// TODO: get commit message from the command line
-
-async function gitUpdate(msg){
+async function gitUpdate(msg) {
   await $`git add -A`;
   await $`git commit -m ${msg}`;
   await $`git push`;
 }
 
+var theArgs = process.argv.slice(2);
+
 console.log('\nStarting project publish...');
 
 // Check the command line arguments to see if we should increment the version
-if (process.argv.includes('-i')) {
+let idx = theArgs.indexOf('-i');
+if (idx > -1) {
+  // remove the -i argument from the array
+  theArgs = theArgs.splice(idx, 1);
   console.log('\nIncrementing package version');
+
   await gitUpdate('Incrementing package version');
   await $`npm version patch`;
 } else {
   console.log('Skipping package version increment');
 }
 
+console.dir(theArgs);
+
 // Check the command line arguments to see if we should update the Algolia index
-if (process.argv.includes('-a')) {
+idx = theArgs.indexOf('-a');
+if (idx > -1) {
+  // remove the -a argument from the array
+  theArgs = theArgs.splice(idx, 1);
   console.log('\nUpdating Algolia Index');
   await $`algolia-idxup`;
 } else {
   console.log('Skipping Algolia index update');
 }
+
+console.dir(theArgs);
+
+process.exit(0);
 
 console.log();  // throw in a blank line on the console
 await $`gen-build-info src/_data`;
