a4678a3562056b32f545036153a0680b5359191c
John M. Wargo <johnwargo@users.noreply.github.com>
2023-04-28 19:59:30 -0400

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -203,7 +203,7 @@ module.exports = eleventyConfig => {
 	// Assumes cascading folders per year
 	// let thisYear = new Date().getFullYear();
 	// for (let i = 2009; i <= thisYear; i++) {
-		for (let i = 2009; i <= 2022; i++) {
+		for (let i = 2009; i <= 2021; i++) {
 		eleventyConfig.addPassthroughCopy(`src/images/${i}/*`);
 	}
 
