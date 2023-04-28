6cd828c6b7e109b86010de51fca76f27044746a5
John M. Wargo <johnwargo@users.noreply.github.com>
2023-04-28 07:34:21 -0400

    Updating the pub command

--- pub.mjs
+++ pub.mjs
@@ -6,17 +6,20 @@ async function gitUpdate(msg) {
   await $`git push`;
 }
 
-var theArgs = process.argv.slice(2);
+// [
+//   'C:\\Program Files\\nodejs\\node.exe',
+//   'C:\\Users\\john\\AppData\\Roaming\\npm\\node_modules\\zx\\build\\cli.js',
+//   'pub.mjs'
+// ]
+var theArgs = process.argv.slice(3);
 
 console.log('\nStarting project publish...');
 
 // Check the command line arguments to see if we should increment the version
 let idx = theArgs.indexOf('-i');
 if (idx > -1) {
-  // remove the -i argument from the array
-  theArgs = theArgs.splice(idx, 1);
+  theArgs = theArgs.splice(idx, 1);   // remove the -i argument from the array
   console.log('\nIncrementing package version');
-
   await gitUpdate('Incrementing package version');
   await $`npm version patch`;
 } else {
@@ -26,27 +29,30 @@ if (idx > -1) {
 // Check the command line arguments to see if we should update the Algolia index
 idx = theArgs.indexOf('-a');
 if (idx > -1) {
-  // remove the -a argument from the array
-  theArgs = theArgs.splice(idx, 1);
+  theArgs = theArgs.splice(idx, 1); // remove the -a argument from the array
   console.log('\nUpdating Algolia Index');
   await $`algolia-idxup`;
 } else {
   console.log('Skipping Algolia index update');
 }
 
-console.dir(theArgs);
-
+// Do we have a commit message?
 if (theArgs.length === 0) {
-  console.log('Missing commit message on command line');
+  console.log('\nMissing commit message on command line');
   process.exit(0);
 }
 
-process.exit(0);
+// Do we have too many command line arguments?
+if (theArgs.length > 1) {
+  console.log('\nToo many command line arguments, make sure the commit message is in quotes');
+  process.exit(0);
+}
 
-console.log();  // throw in a blank line on the console
+// throw in a blank line on the console
+console.log();  
 await $`gen-build-info src/_data`;
 await $`11ty-cat-pages`;
 console.log('\nBuilding site');
 await $`eleventy`;
 
-gitUpdate(`Update ${new Date().toLocaleString()}`);
+gitUpdate(theArgs[0]);
