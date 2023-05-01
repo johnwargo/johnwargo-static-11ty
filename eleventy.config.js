9ed7548d310a790dff30e505ce9f92e3a703f75d
John M. Wargo <johnwargo@users.noreply.github.com>
2023-05-01 17:08:06 -0400

    Many things...
    Updated Feed
    Added JSON Feed
    Moved images to separate file

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,14 +1,14 @@
 const { EleventyHtmlBasePlugin } = require('@11ty/eleventy');
-const Image = require('@11ty/eleventy-img');
-const path = require('path');
 // https://github.com/11ty/eleventy/issues/2301
 const markdownIt = require('markdown-it');
 const markdownItAttrs = require('markdown-it-attrs');
-const outdent = require('outdent');
 const pluginDate = require('eleventy-plugin-date');
 const pluginRss = require('@11ty/eleventy-plugin-rss');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const embedYouTube = require('eleventy-plugin-youtube-embed');
+
+// local plugins
+const pluginImages = require("./eleventy.config.images.js");
 
 // Transforms
 // https://learneleventyfromscratch.com/lesson/31.html#minifying-html-output
@@ -24,6 +24,7 @@ module.exports = eleventyConfig => {
 	eleventyConfig.addPlugin(pluginDate);
 	eleventyConfig.addPlugin(pluginRss);
 	eleventyConfig.addPlugin(syntaxHighlight);
+	eleventyConfig.addPlugin(pluginImages);
 
 	// https://github.com/11ty/eleventy/issues/2301
 	const mdOptions = {
@@ -62,88 +63,6 @@ module.exports = eleventyConfig => {
 		return post.templateContent;
 	}
 
-	// ==============================================
-	// Image Shortcode
-	// ==============================================
-	// https://www.aleksandrhovhannisyan.com/blog/eleventy-image-plugin/
-
-	/** Maps a config of attribute-value pairs to an HTML string
- * representing those same attribute-value pairs.
- */
-	const stringifyAttributes = (attributeMap) => {
-		return Object.entries(attributeMap)
-			.map(([attribute, value]) => {
-				if (typeof value === 'undefined') return '';
-				return `${attribute}="${value}"`;
-			})
-			.join(' ');
-	};
-
-	const imageShortcode = async (
-		src,
-		alt,
-		className = undefined,
-		widths = [400, 800, 1280],
-		formats = ['webp', 'jpeg'],
-		sizes = '100vw'
-	) => {
-		const imageMetadata = await Image(src, {
-			widths: [...widths, null],
-			formats: [...formats, null],
-			outputDir: '_site/img',
-			urlPath: '/img',
-		});
-
-		const sourceHtmlString = Object.values(imageMetadata)
-			// Map each format to the source HTML markup
-			.map((images) => {
-				// The first entry is representative of all the others
-				// since they each have the same shape
-				const { sourceType } = images[0];
-				// Use our util from earlier to make our lives easier
-				const sourceAttributes = stringifyAttributes({
-					type: sourceType,
-					// srcset needs to be a comma-separated attribute
-					srcset: images.map((image) => image.srcset).join(', '),
-					sizes,
-				});
-				// Return one <source> per format
-				return `<source ${sourceAttributes}>`;
-			})
-			.join('\n');
-
-		const getLargestImage = (format) => {
-			const images = imageMetadata[format];
-			return images[images.length - 1];
-		}
-
-		const largestUnoptimizedImg = getLargestImage(formats[0]);
-
-		const imgAttributes = stringifyAttributes({
-			src: largestUnoptimizedImg.url,
-			// Added classname here because the scott.css file needs it on the img tag
-			class: className,
-			width: largestUnoptimizedImg.width,
-			// removed because it was messing with the aspect ratio
-			// height: largestUnoptimizedImg.height,
-			alt,
-			loading: 'lazy',
-			decoding: 'async',
-		});
-
-		const imgHtmlString = `<img ${imgAttributes}>`;
-
-		const pictureAttributes = stringifyAttributes({
-			class: className,
-		});
-		
-		const picture = `<picture ${pictureAttributes}> ${sourceHtmlString} ${imgHtmlString}</picture>`;
-
-		return outdent`${picture}`;
-	};
-	eleventyConfig.addShortcode('image', imageShortcode);
-	// ==============================================
-
 	eleventyConfig.addCollection("categories", function (collectionApi) {
 		let categories = new Set();
 		let posts = collectionApi.getFilteredByTag('post');
@@ -152,10 +71,6 @@ module.exports = eleventyConfig => {
 			cats.forEach(c => categories.add(c));
 		});
 		return Array.from(categories);
-	});
-
-	eleventyConfig.addCollection("topten", function (collectionApi) {
-		return [];
 	});
 
 	// https://www.raymondcamden.com/2020/06/24/adding-algolia-search-to-eleventy-and-netlify
---
+++ src/_data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "John M. Wargo",
+	url: "https://johnwargo.com/",
+	language: "en",
+	description: "Tech tidbits and other stuff",
+	author: {
+		name: "John M. Wargo",
+		email: "&#106;&#111;&#104;&#110;&#064;&#106;&#111;&#104;&#110;&#119;&#097;&#114;&#103;&#111;&#046;&#099;&#111;&#109;",
+		url: "https://johnwargo.com"
+	}
+}
