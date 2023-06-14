9e613eeb86bcf5c974c74260736b4d86c93117b7
John M. Wargo <johnwargo@users.noreply.github.com>
2023-06-14 09:26:23 -0400

    nav changes, reading list updates, header formatting

--- src/assets/js/goodreads.js
+++ src/assets/js/goodreads.js
@@ -3,19 +3,20 @@
   let bookDiv = document.getElementById('readingList');
   const response = await fetch("https://goodreads-5bfb4vi5ia-ue.a.run.app/", { mode: 'cors' });
   const res = await response.json();
-  let content='';
+  let content = '<p>';
   if (res.readingList.length > 0) {
-    content = "<p>Books I'm currently reading (<a href=\"https://www.goodreads.com/user/show/51500942-john-wargo\" target=\"_blank\">Goodreads</a>):</p>";
+    content = (res.readingList.length < 2) ? "The book" : "Books";
+    content += " I'm currently reading <a href=\"https://www.goodreads.com/user/show/51500942-john-wargo\" target=\"_blank\">according to Goodreads</a>:</p>";
     content += "<ul class=\"posts\">";
     res.readingList.forEach(function (book) {
       content += "<li>";
       content += `<a href="${book.link}" target="_blank"><img src="${book.images.medium}" alt="${book.title}" /></a><br />`;
       content += `<a href="${book.link}" target="_blank">${book.title}</a><br /> by ${book.author}`
-      content += "</li>";      
+      content += "</li>";
     });
     content += "</ul>";
   } else {
     content = "<p>Goodreads feed not available (or empty).</p>";
-  }  
+  }
   bookDiv.innerHTML = content;
 })();
