bb0287b47ba08a9cac90443a26d5fa473649abff
John M. Wargo <johnwargo@users.noreply.github.com>
2024-01-06 11:27:42 -0500

    working

--- src/_data/topPosts.js
+++ src/_data/topPosts.js
@@ -1,5 +1,6 @@
-(async function () {
+'use strict'
 
+module.exports = async function () {
   const feedURL = 'https://us-east1-jmw-static-site.cloudfunctions.net/getTopPosts';
 
   const response = await fetch(feedURL, { mode: 'cors' });
@@ -12,4 +13,4 @@
     console.error(`popularArticles Error: ${response.status} - ${response.statusText}`);
     process.exit(1);
   }
-})();
+}
