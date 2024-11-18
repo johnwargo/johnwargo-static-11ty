7821de31aa0cdaaf66f12374f36e610dc469085a
johnwargo <john@johnwargo.com>
2024-11-18 14:41:14 -0500

    Deleted WWA posts section from home page

--- src/_data/wwa-posts.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-module.exports = async function () {
-  const response = await fetch('https://aworldwithoutapps.com/feed.json');
-  const data = await response.json();
-  return data.items;
-}
